2bc955cf47ae22a4ff09a1fcac0aed23d266c2d5
John Collier <John.J.Collier@ibm.com>
2019-11-11 17:19:35 -0500

    Update build.sh

--- codewind-che-sidecar/build.sh
+++ codewind-che-sidecar/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #*******************************************************************************
 # Copyright (c) 2019 IBM Corporation and others.
@@ -11,6 +11,12 @@
 #     IBM Corporation - initial API and implementation
 #*******************************************************************************
 
+# README FIRST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 
+# NOTE: change of this file should be in sync with 'Jenkinsfile(stage: Build Docker images)'
+# Ping kjoseph@ca.ibm.com for details
+#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 # Move to the directory of the script that included this include file ------
 export SCRIPT_LOCT=$( cd $( dirname $0 ); pwd )
@@ -21,6 +27,32 @@ if [ -d "codewind-filewatchers" ]; then
   rm -rf codewind-filewatchers
 fi
 
+INSTALLER_REPO="https://github.com/eclipse/codewind-installer.git"
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+
 git clone https://github.com/eclipse/codewind-filewatchers.git
 
-docker build -t codewind-che-sidecar .
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+printf "\n${BLUE}Warning: Verify that the installer target branch below is the branch you expect to be targeting with your build${NC}\n"
+sleep 1
+
+# the command below will echo the head commit if the branch exists, else it just exits
+if [[ -n $(git ls-remote --heads $INSTALLER_REPO ${BRANCH_NAME}) ]]; then
+    echo "Will use matching ${BRANCH_NAME} branch on $INSTALLER_REPO"
+    export CW_CLI_BRANCH=${BRANCH_NAME}
+else
+    export CW_CLI_BRANCH=master
+    source scripts/installer-branch-override.env
+    echo "No matching branch on $INSTALLER_REPO - using $CW_CLI_BRANCH branch. Override this in installer-branch-override.env"
+fi
+
+
+echo
+
+if [[ -z "$BUILDAH" ]]; then
+  docker build --build-arg CW_CLI_BRANCH="$CW_CLI_BRANCH" -t codewind-che-sidecar .
+else
+  buildah bud --build-arg CW_CLI_BRANCH="$CW_CLI_BRANCH" -t codewind-che-sidecar .
+fi
